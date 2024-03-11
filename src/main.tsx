c969586540341a7133e50478eac8859668c9fee0
adityaisingh <ishu9918663060@gmail.com>
2024-03-11 09:52:10 +0530

    auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import Auth0ProviderWithNavigate from "./auth/Auth0providerwithNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRouter />
+      <Auth0ProviderWithNavigate>
+        <AppRouter />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
